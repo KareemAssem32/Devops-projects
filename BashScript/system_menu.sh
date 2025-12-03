305be21cc785239224627ac0b0f53b0086ae49ce
KareemAssem32 <kareemasim00@gmail.com>
2025-12-03 20:22:38 +0200

    Project

---
+++ BashScript/system_menu.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#OUTPUT
+echo "1)Show Date And Time"
+echo "2)Display Current User"
+echo "3)Show Current Directory"
+echo "4)List Files in Current Directory"
+echo "5)Display Disk Usage"
+echo "6)Exit"
+
+#INPUT
+
+read -p "Choose The Service: " num
+
+if [[ "${num}" =~ ^[0-9]+$ ]]; then
+	echo "------------------- "
+else 
+	echo "Enter Service Number!"
+exit 1
+	fi
+
+
+#SERVICE
+
+case $num in 
+	1) 
+	echo " The Date And Time Is: $(date) "
+		;;
+	2)
+		echo " The Current User is: $(whoami)"
+		;;
+	3)
+		echo " The Current Directory Is: $(pwd) "
+		;;
+	4)
+		echo "List OF Files And Directory: "
+		echo "-----------------------------"
+		echo " $(ls -a) "
+		;;
+	5)
+		echo "Disk Usage"
+		echo "----------"
+		echo " $(df) "
+		;;
+	6) 
+		exit 0
+
+		;;
+	*)
+		echo "No Sevices Have This Number!"
+		;;
+esac
